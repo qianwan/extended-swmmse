ce11c87ebf7e08b660797eadc7f2967262722330
qianwan <qianwan@ymail.com>
2013-10-12 17:25:44 +0800

    wmmse

--- wmmse.m
+++ wmmse.m
@@ -1,37 +1,40 @@
 K = 3;
-M = 2;
-N = 2;
+M = 1;
+N = 1;
 Q = 1;
 I = 1;
 SNRdB = 25;
 SNR = 10^(SNRdB / 10);
 P = SNR;
 clusterLocations = [0 + 0j, 0 + 2000j, 0 - 2000j];
-clusterClosures = [1; 2; 3];
+clusterClosures = [1 : 1 : 3]';
+%clusterLocations = [0 + 0j, 0 + 2000j, 0 - 2000j, 0 + 4000j, 0 - 4000j, 0 + 6000j, 0 - 6000j, 0 + 8000j, 0 - 8000j, 0 + 10000j];
+%clusterClosures = [1 : 1 : 10]';
 [bsLocations, ueLocations] = brownian(2000 / sqrt(3), clusterLocations, Q, I);
 
 numCases = 100;
 totalSumRate = 0;
 totalNumIterations = 0;
-maxIterations = 1000;
-epsilon = 1e-2;
+maxIterations = 1e6;
+epsilon = 1e-3;
 for i = 1 : numCases
   numIterations = 0;
   prev = 0.0;
   H = generateMIMOChannel(K, Q, M, bsLocations, I, N, ueLocations);
   V = generateRandomTxVector(K, Q, M, I, P, clusterClosures);
   [U, W, R] = updateWMMSEVariables(K, Q, M, I, N, H, V);
-  while abs(prev - sum(W)) > epsilon
-    prev = sum(W);
+  while abs(prev - sum(R)) > epsilon
+    prev = sum(R);
     numIterations = numIterations + 1;
     if numIterations > maxIterations
+      numIterations = numIterations - 1;
       break;
     end
     mmse = updateMmseMMatrix(K, Q, M, I, N, H, U, W);
     V = iterateWMMSE(K, Q, M, I, N, mmse, P, H, W, U);
     [U, W, R] = updateWMMSEVariables(K, Q, M, I, N, H, V);
   end
-  fprintf(2, 'Case #%d: sum rate = %f, #iterations%d\n', i, sum(R), numIterations);
+  fprintf(2, 'Case #%d: R = %f, # = %d\n', i, sum(R), numIterations);
   totalSumRate = totalSumRate + sum(R);
   totalNumIterations = totalNumIterations + numIterations;
 end

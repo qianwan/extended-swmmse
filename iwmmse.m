c57480b80d6e2e24df4d3407ed80fa7ad2ebe32c
Qian Wan <qianwan@ymail.com>
2013-10-28 20:23:33 +0800

    TrH and TrV for iwmmse

--- iwmmse.m
+++ iwmmse.m
@@ -24,6 +24,8 @@ totalSumRate = 0;
 totalNumIterations = 0;
 maxIterations = 1e6;
 epsilon = 1e-1;
+TrH = zeros(numCases * K * I * Q, 1);
+TrV = zeros(numCases * K * I * Q, 1);
 for i = 1 : numCases
     numIterations = 0;
     prev = 0.0;
@@ -48,6 +50,20 @@ for i = 1 : numCases
     totalNumIterations = totalNumIterations + numIterations;
     fprintf(2, '=>Current avg sum rate: %f\n', totalSumRate / i);
     fprintf(2, '=>Current avg number of iterations: %f\n', totalNumIterations / i);
+    for k = 1 : K
+        for j = 1 : I
+            for q = 1 : Q
+                rowOffset = (k - 1) * I * N + (j - 1) * N;
+                colOffset = (k - 1) * Q * M + (q - 1) * M;
+                h = H(rowOffset + 1 : rowOffset + N, colOffset + 1 : colOffset + M);
+                TrH((i - 1) * K * I * Q + (k - 1) * I * Q + (j - 1) * Q + q) = trace(h * h');
+                rowOffset = (k - 1) * Q * M + (q - 1) * M;
+                colOffset = (k - 1) * I + j;
+                v = V(rowOffset + 1 : rowOffset + M, colOffset);
+                TrV((i - 1) * K * I * Q + (k - 1) * I * Q + (j - 1) * Q + q) = norm(v);
+            end
+        end
+    end
 end
 fprintf(2, 'Avg sum rate: %f\n', totalSumRate / numCases);
 fprintf(2, 'Avg number of iterations: %f\n', totalNumIterations / numCases);
